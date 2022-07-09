c55028b7c80178aea952b48eb07d1434b2ee6264
svilen iordanov <svilenborianov@gmail.com>
2022-07-09 12:32:40 +0200

    Login form

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import {NavBar} from "../Components/Common/NavBar"
 import {Register} from "../Components/User/Register"
+import {Login} from "../Components/User/Login"
 
 function App() {
     return (
         <div>
             <NavBar/>
             <Register/>
+            <Login/>
         </div>
     );
 }
