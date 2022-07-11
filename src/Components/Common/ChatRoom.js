2fd5c3d2b6db1756ac5f8bf7326d0cff001e95be
svilen iordanov <svilenborianov@gmail.com>
2022-07-11 11:12:13 +0200

    Chat Room v2.0

--- src/Components/Common/ChatRoom.js
+++ src/Components/Common/ChatRoom.js
@@ -4,17 +4,24 @@ export const ChatRoom = () => {
     return (
         <div>
             <div className="Chat">
+                <p className="Message"><b>Name: Message</b></p>
+                <p className="Message"><b>Name: Message</b></p>
+                <p className="Message"><b>Name: Message</b></p>
+                <p className="Message"><b>Name: Message</b></p>
+                <p className="Message"><b>Name: Message</b></p>
+                <p className="Message"><b>Name: Message</b></p>
             </div>
 
             <div className="Keyboard">
-                <textarea className="TextArea"></textarea>
+                <textarea className="TextArea" placeholder="Enter Message Here"></textarea>
             </div>
 
             <div className="DivButton">
                 <button className="SendButton">SEND</button>
             </div>
-            
+
             <div className="Emojis">
+                <p>Some emojis will be here</p>
             </div>
 
         </div>
