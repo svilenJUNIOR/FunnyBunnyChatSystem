f0c6121882eaec7d2b3ee578974ff12a24916ee0
svilen iordanov <svilenborianov@gmail.com>
2022-07-11 11:06:00 +0200

    Chat room

--- src/Components/Common/ChatRoom.js
+++ src/Components/Common/ChatRoom.js
@@ -1,5 +1,22 @@
 import "../Common/Styles/ChatRoom.css"
 
 export const ChatRoom = () => {
-    return(<h1>yesa</h1>);
+    return (
+        <div>
+            <div className="Chat">
+            </div>
+
+            <div className="Keyboard">
+                <textarea className="TextArea"></textarea>
+            </div>
+
+            <div className="DivButton">
+                <button className="SendButton">SEND</button>
+            </div>
+            
+            <div className="Emojis">
+            </div>
+
+        </div>
+    );
 }
