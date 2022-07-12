7948d7b72744630aac43eaa7fa41f575666c5783
svilen iordanov <svilenborianov@gmail.com>
2022-07-12 14:37:00 +0200

    Minor styles changes

--- src/Components/User/Login.js
+++ src/Components/User/Login.js
@@ -2,34 +2,36 @@ import "./Styles/Login.css"
 
 export const Login = () => {
     return (
-        <form method="POST">
-            <table className="LoginTable">
-                <tbody>
-                <tr>
-                    <td>
-                        <label htmlFor="Email"><b>Email</b></label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Enter Email" name="Email" required/>
-                    </td>
-                </tr>
-    
-                <tr>
-                    <td>
-                        <label htmlFor="Password"><b>Password</b></label>
-                    </td>
-                    <td>
-                        <input type="password" placeholder="Enter Password" name="Password" required/>
-                    </td>
-                </tr>
-    
-                <tr>
-                    <td colSpan={2}>
-                        <button type="submit">Log In</button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </form>
+        <div className="WraperLogin">
+            <form method="POST">
+                <table className="LoginTable">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <label htmlFor="Email"><b>Email</b></label>
+                            </td>
+                            <td>
+                                <input type="text" placeholder="Enter Email" name="Email" required />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <label htmlFor="Password"><b>Password</b></label>
+                            </td>
+                            <td>
+                                <input type="password" placeholder="Enter Password" name="Password" required />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan={2}>
+                                <button type="submit">Log In</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
     );
 }
--- src/Components/User/Register.js
+++ src/Components/User/Register.js
@@ -2,71 +2,73 @@ import "./Styles/Register.css";
 
 export const Register = () => {
     return (
-        <form method="POST">
-            <table className="RegisterTable">
-                <tbody>
-                    <tr>
-                        <td>
-                            <label htmlFor="Email"><b>Email</b></label>
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Enter Email" name="Email" required />
-                        </td>
+        <div className="WraperRegister">
+            <form method="POST">
+                <table className="RegisterTable">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <label htmlFor="Email"><b>Email</b></label>
+                            </td>
+                            <td>
+                                <input type="text" placeholder="Enter Email" name="Email" required />
+                            </td>
 
-                        <td>
-                            <label htmlFor="Password"><b>Password</b></label>
-                        </td>
-                        <td>
-                            <input type="password" placeholder="Enter Password" name="Password" required />
-                        </td>
-                    </tr>
+                            <td>
+                                <label htmlFor="Password"><b>Password</b></label>
+                            </td>
+                            <td>
+                                <input type="password" placeholder="Enter Password" name="Password" required />
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td>
-                            <label htmlFor="Name"><b>Name</b></label>
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Enter Name" name="Name" required />
-                        </td>
+                        <tr>
+                            <td>
+                                <label htmlFor="Name"><b>Name</b></label>
+                            </td>
+                            <td>
+                                <input type="text" placeholder="Enter Name" name="Name" required />
+                            </td>
 
-                        <td>
-                            <label htmlFor="HairColor"><b>Hair Color</b></label>
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Enter Hair color" name="HairColor" required />
-                        </td>
-                    </tr>
+                            <td>
+                                <label htmlFor="HairColor"><b>Hair Color</b></label>
+                            </td>
+                            <td>
+                                <input type="text" placeholder="Enter Hair color" name="HairColor" required />
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td>
-                            <label htmlFor="Breed"><b>Breed</b></label>
-                        </td>
-                        <td>
-                            <select className="Breed">
-                                <option value="Default" defaultValue>Not Selected</option>
-                                <option value="DutchDwarf">Duch Dwarf</option>
-                                <option value="LionHead">Lion Head</option>
-                                <option value="BelgiumGiant">Belgium Giant</option>
-                                <option value="WildBreed">Wild Breed</option>
-                            </select>
-                        </td>
+                        <tr>
+                            <td>
+                                <label htmlFor="Breed"><b>Breed</b></label>
+                            </td>
+                            <td>
+                                <select className="Breed">
+                                    <option value="Default" defaultValue>Not Selected</option>
+                                    <option value="DutchDwarf">Duch Dwarf</option>
+                                    <option value="LionHead">Lion Head</option>
+                                    <option value="BelgiumGiant">Belgium Giant</option>
+                                    <option value="WildBreed">Wild Breed</option>
+                                </select>
+                            </td>
 
-                        <td>
-                            <label htmlFor="SignatureJoke"><b>Signature Joke</b></label>
+                            <td>
+                                <label htmlFor="SignatureJoke"><b>Signature Joke</b></label>
 
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Enter Signature Joke" name="SignatureJoke" required />
-                        </td>
-                    </tr>
+                            </td>
+                            <td>
+                                <input type="text" placeholder="Enter Signature Joke" name="SignatureJoke" required />
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td colSpan={2}>
-                            <button type="submit">Sign Up</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </form>
+                        <tr>
+                            <td colSpan={2}>
+                                <button type="submit">Sign Up</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
     );
 }
