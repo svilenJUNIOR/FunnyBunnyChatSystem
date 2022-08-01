8ab5aa5465b63077d5e8614146526471f25186af
svilen iordanov <svilenborianov@gmail.com>
2022-08-01 12:17:34 +0200

    Text message

--- src/Components/Bunnies/ChatRoom.js
+++ src/Components/Bunnies/ChatRoom.js
@@ -1,31 +1,42 @@
 import "./Styles/ChatRoom.css"
 import { useLocation } from "react-router-dom"
+import { useState } from "react"
 
 export const ChatRoom = (value) => {
 
     var location = useLocation();
 
-   console.log(location.state.ChatName);
+    var [msg, setMsg] = useState({ Message: "", });
 
+    var ChangeHandler = (e) => {
+        setMsg(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    var SubmitHandler = async (e) => {
+        e.preventDefault();
+        console.log(`${location.state.ChatName} -  ${msg.Message}`)
+    };
 
     return (
         <div>
             <div className="Chat">
                 <p className="Message"><b>Name: Message</b></p>
-                <p className="Message"><b>Name: Message</b></p>
-                <p className="Message"><b>Name: Message</b></p>
-                <p className="Message"><b>Name: Message</b></p>
-                <p className="Message"><b>Name: Message</b></p>
-                <p className="Message"><b>Name: Message</b></p>
             </div>
 
-            <div className="Keyboard">
-                <textarea className="TextArea" placeholder="Enter Message Here"></textarea>
-            </div>
+            <form onSubmit={SubmitHandler}>
 
-            <div className="DivButton">
-                <button className="SendButton">SEND</button>
-            </div>
+                <div className="Keyboard">
+                    <label htmlFor="Message"><b>Message</b></label>
+                    <textarea name="Message" className="TextArea" placeholder="Enter Message Here" value={msg.Message} onChange={ChangeHandler} required></textarea>
+                </div>
+                <div className="DivButton">
+                    <button type="submit" className="SendButton">SEND</button>
+                </div>
+
+            </form>
 
             <div className="Emojis">
                 <p>Some emojis will be here</p>
